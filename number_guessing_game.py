9e4e461c26ca483e964b1dcf7f1b3ef37af88aff
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-11-27 17:03:51 -0500

    removed pseudo code and tested code

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,19 +1,19 @@
 import random
 
 def guess_number():
-    # Generate a random number between 0 and 100
+   
     target_number = random.randint(0, 100)
 
     while True:
-        # Ask the user to guess the number
+        
         user_guess = int(input("Guess a number between 0 and 100!: "))
 
-        # Check if the guess is correct
+        
         if user_guess == target_number:
             print("Winner!")
             play_again = input("Would you like to play again? (y or n) ").lower()
             
-            # If the user wants to play again, generate a new random number
+          
             if play_again == 'y':
                 target_number = random.randint(0, 100)
             else:
@@ -22,5 +22,5 @@ def guess_number():
         else:
             print("Incorrect. Try again.")
 
-# Start the game
+
 guess_number()
